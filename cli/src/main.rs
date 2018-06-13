67833c7b061d26f2ec568e4affaa42dd3bdc13c1
proelbtn <proelbtn@users.noreply.github.com>
2018-06-13 22:24:15 +0900

    [ADD] SimplePlayerとSimpleEnemyをそれとなく実装した。

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,106 @@
+extern crate librobots;
+
+use librobots::{Robots, PlayerTrait, EnemyTrait, Operations, Vec2};
+
+struct SimplePlayer {
+    pos: Vec2
+}
+
+impl SimplePlayer {
+    fn new(x: u64, y: u64) -> SimplePlayer {
+        SimplePlayer { pos: Vec2::new(x, y) }
+    }
+}
+
+impl PlayerTrait for SimplePlayer {
+    fn pos(&self) -> Vec2 { self.pos }
+
+    fn next(&mut self, size: Vec2, op: Operations, enemies: &Vec<Box<EnemyTrait>>) {
+        let upper_bound = self.pos.y == 0;
+        let lower_bound = self.pos.y == size.y;
+        let left_bound = self.pos.x == 0;
+        let right_bound = self.pos.x == size.x;
+
+        let mut pos = self.pos;
+        let (ex, ey) = (Vec2::new(1, 0), Vec2::new(0, 1));
+
+        match op {
+            Operations::Up() => {
+                if upper_bound { return; }
+                pos = self.pos - ey;
+            }
+            Operations::Down() => {
+                if upper_bound { return; }
+                pos = self.pos + ey;
+            }
+            Operations::Left() => {
+                if upper_bound { return; }
+                pos = self.pos - ex;
+            }
+            Operations::Right() => {
+                if upper_bound { return; }
+                pos = self.pos + ex;
+            }
+            Operations::UpperLeft() => {
+                if upper_bound { return; }
+                pos = self.pos - ex - ey;
+            }
+            Operations::UpperRight() => {
+                if upper_bound { return; }
+                pos = self.pos + ex - ey;
+            }
+            Operations::LowerLeft() => {
+                if upper_bound { return; }
+                pos = self.pos - ex + ey;
+            }
+            Operations::LowerRight() => {
+                if upper_bound { return; }
+                pos = self.pos + ex + ey;
+            }
+            Operations::Warp() => {
+            }
+            _ => ()
+        }
+
+        self.pos = pos;
+    }
+}
+
+struct SimpleEnemy {
+    pos: Vec2
+}
+
+impl SimpleEnemy {
+    fn new(x: u64, y: u64) -> SimpleEnemy {
+        SimpleEnemy { pos: Vec2::new(x, y) }
+    }
+}
+
+impl EnemyTrait for SimpleEnemy {
+    fn id(&self) -> u64 { 1 }
+    fn pos(&self) -> Vec2 { self.pos }
+
+    fn next(&mut self, size: Vec2, player: &Box<PlayerTrait>) {
+        let (px, py) = (player.pos().x, player.pos().y);
+        let mut pos = self.pos;
+        let (ex, ey) = (Vec2::new(1, 0), Vec2::new(0, 1));
+
+        if px < self.pos.x { pos = pos - ex; }
+        if px > self.pos.x { pos = pos + ex; }
+        if py < self.pos.y { pos = pos - ey; }
+        if py > self.pos.y { pos = pos + ey; }
+
+        self.pos = pos;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let size = Vec2::new(25, 15);
+    let mut p: Box<PlayerTrait> = Box::new(SimplePlayer::new(size.x / 2, size.y / 2));
+    let mut es: Vec<Box<EnemyTrait>> = Vec::new();
+    for n in 0..5 {
+        es.push(Box::new(SimpleEnemy::new(n, n))); 
+    }
+
+    let g = Robots::new(size, &mut p, &mut es);
 }
