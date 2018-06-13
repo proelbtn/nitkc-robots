b313f9359af94f1b8f3f9bf3cae06e8eff587649
proelbtn <proelbtn@users.noreply.github.com>
2018-06-13 22:22:40 +0900

    [ADD] Vec2に加減算のTraitを実装した。

---
+++ librobots/src/vec2.rs
@@ -0,0 +1,29 @@
+use std::ops::{Add, Sub};
+
+#[derive(Clone, Copy)]
+pub struct Vec2 {
+    pub x: u64,
+    pub y: u64,
+}
+
+impl Vec2 {
+    pub fn new(x: u64, y: u64) -> Vec2 {
+        Vec2 { x, y }
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, other: Vec2) -> Vec2 {
+        Vec2::new(self.x + other.x, self.y + other.y)
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Vec2;
+
+    fn sub(self, other: Vec2) -> Vec2 {
+        Vec2::new(self.x - other.x, self.y - other.y)
+    }
+}
